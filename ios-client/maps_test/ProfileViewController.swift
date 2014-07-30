17a7734225a3541cc54fdf355833bacb420069df
Сергей <zydins@gmail.com>
2014-07-30 23:12:16 +0400

    ios-client: profile and navigator color added

---
+++ ios-client/maps_test/ProfileViewController.swift
@@ -0,0 +1,13 @@
+//
+//  ProfileViewController.swift
+//  trip-planner
+//
+//  Created by Сергей on 29.07.14.
+//  Copyright (c) 2014 tryparse. All rights reserved.
+//
+
+import Cocoa
+
+class ProfileViewController: UIViewController {
+
+}
