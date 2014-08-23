9d022100bb815906d99c4f49ea16b8e50030411d
Сергей <zydins@gmail.com>
2014-08-23 18:53:43 +0400

    ios-client: version 0.2. Added editing on city page and edititng view with day picker, changed mechanism of showing view with map

---
+++ ios-client/maps_test/CityEditViewController.swift
@@ -0,0 +1,35 @@
+//
+//  CityEditViewController.swift
+//  trip-planner
+//
+//  Created by Сергей on 23.08.14.
+//  Copyright (c) 2014 tryparse. All rights reserved.
+//
+
+import UIKit
+
+class CityEditViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // #pragma mark - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue!, sender: AnyObject!) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
