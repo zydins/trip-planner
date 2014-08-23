9f3a06104e384461a8b7ba44bd1327850dc7ca5e
Сергей <zydins@gmail.com>
2014-08-23 16:21:09 +0400

    search page and markers added

---
+++ ios-client/maps_test/DataStructures.swift
@@ -0,0 +1,9 @@
+//
+//  DataStructures.swift
+//  trip-planner
+//
+//  Created by Сергей on 01.08.14.
+//  Copyright (c) 2014 tryparse. All rights reserved.
+//
+
+import Foundation
---
+++ ios-client/maps_test/PlacesSearchViewController.swift
@@ -0,0 +1,13 @@
+//
+//  PlacesSearchViewController.swift
+//  trip-planner
+//
+//  Created by Сергей on 01.08.14.
+//  Copyright (c) 2014 tryparse. All rights reserved.
+//
+
+import Cocoa
+
+class PlacesSearchViewController: UITableViewController {
+
+}
