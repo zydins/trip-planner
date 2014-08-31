4fc4d8f4f2a9350f021d4a578e2bbeb980350de5
Сергей <zydins@gmail.com>
2014-09-01 00:20:35 +0400

    ios-client: search places by city, add new place on the map, remove place

---
+++ ios-client/maps_test/TAAnnotation.h
@@ -0,0 +1,13 @@
+//
+//  TAAnnotation.h
+//  trip-planner
+//
+//  Created by Сергей on 29.08.14.
+//  Copyright (c) 2014 creators. All rights reserved.
+//
+
+#import "RMAnnotation.h"
+
+@interface TAAnnotation : RMAnnotation
+
+@end
---
+++ ios-client/maps_test/TAAnnotation.m
@@ -0,0 +1,13 @@
+//
+//  TAAnnotation.m
+//  trip-planner
+//
+//  Created by Сергей on 29.08.14.
+//  Copyright (c) 2014 creators. All rights reserved.
+//
+
+#import "TAAnnotation.h"
+
+@implementation TAAnnotation
+
+@end
